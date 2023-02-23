bd160f916adc3873ad33b699981a64d47f756cce
xzhy324 <xzhy324@qq.com>
2023-02-23 20:03:29 +0800

    bug fix update_subscribes.sh

--- update_subscribes.sh
+++ update_subscribes.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # set variable below to your subscribe link for clashx
-subscribe="your.clash.subscribe.link.here"
+# your.clash.subscribe.link.here
+subscribe="https://to.runba.cyou/link/QRHsrJELGlvmL4qT?clash=1"
 
-cp ~/.config/clash/config.yaml ~/.config/clash/config.yaml.old
-wget -U "Mozilla/6.0" -O ~/.config/clash/config.yaml $subscribe
+CONFIG_PATH=~/.config/clash/config.yaml
+if [ -e $CONFIG_PATH ]; then
+	cp $CONFIG_PATH $CONFIG_PATH.old
+fi
+wget -U "Mozilla/6.0" -O $CONFIG_PATH $subscribe
+if test  ! -s $CONFIG_PATH  -a  -e $CONFIG_PATH.old ; then
+	cp $CONFIG_PATH.old $CONFIG_PATH
+fi
